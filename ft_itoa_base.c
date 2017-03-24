325c8578323244a2bf53a1d010e8860c7c2b05dc
Lijun Wang <wang283808065@gmail.com>
2017-03-24 14:19:42 -0700

    Update ft_itoa_base.c

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -10,86 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
 
-int	count(int tmp, int base)
+int		ft_abs(int nb)
 {
-	int i;
-
-	i = 0;
-	while (tmp >= base)
-	{
-		tmp = tmp / base;
-		i++;
-	}
-	return (i);
-}
-
-char	*revNchar(int *idst, int n, int sign)
-{
-	char *dst;
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-	dst = (char*)malloc(sizeof(char) * (n + 1));
-	while (idst[i] != -1)
-		i++;
-	i--;
-	if (sign == -1)
-	{
-		dst[j] = '-';
-		j++;
-	}
-	while (i >= 0)
-	{
-		if (idst[i] > 9)
-			dst[j] = idst[i] - 10 + 'A';
-		else
-			dst[j] = idst[i] + '0';
-		i--;
-		j++;
-	}
-	dst[i] = '\0';
-	return (dst);
+	if (nb < 0)
+		nb = -nb;
+	return (nb);
 }
 
 char	*ft_itoa_base(int value, int base)
 {
-	int i;
-	int tmp;
-	int *idst;
-	int n;
-	int sign;
-
-	i = 0;
-	if (base == 10 && value >= -2147483647 && value < 0)
-	{
-		sign = -1;
-		value = value * (-1);
-	}
-	else if (base == 10 && value == -2147483648)
-	{
-		char *n;
-		n = "-2147483648";
-		return (n);
-	}
-	else
-		sign = 1;
+	char	*str;
+	int		size;
+	char	*tab;
+	int		flag;
+	int		tmp;
+	flag = 0;
+	size = 0;
+	tab = "0123456789ABCDEF";
+	if (base < 2 || base > 16)
+		return (0);
+	if (value < 0 && base == 10)
+		flag = 1;
 	tmp = value;
-	n = count(tmp, base);
-	idst = (int*)malloc(sizeof(int) * (n + 2));
-	while (value >= base)
+	while (tmp /= base)
+		size++;
+	size = size + flag + 1;
+	str = (char *)malloc(sizeof(char) * size  + 1);
+	str[size] = '\0';
+	if (flag == 1)
+		str[0] = '-';
+	while (size > flag)
 	{
-		idst[i] = value % base;
-		value = value / base;
-		i++;
+		str[size - 1] = tab[ft_abs(value % base)];
+		size--;
+		value /=base;
 	}
-	idst[i] = value;
-	i++;
-	idst[i] = -1;
-	return (revNchar(idst, n, sign));
+	return (str);
 }
